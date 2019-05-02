d9ceee70ba4269d0a6fd8302ab45eab7676249c9
gabriella-quintero-ntig <gabriella.quintero@elev.ga.ntig.se>
2019-05-02 10:21:51 +0200

    Method & action
    Behövde lägga till en method och action för att submit-knappen ska kunna leda till något.

--- app.rb
+++ app.rb
@@ -15,14 +15,33 @@ end
 
 get('/sign') do
     slim(:sign) 
+    #db =SQLite3::Database.new("db/blog.db")
+   # result = db.execute("INSERT INTO user (name, lastname, username, email, password) VALUES (' ', ' ', ' ', ' ', ' ')")
+end
+
+post('/sign') do
+    "Welcome :)"
 end
 
 get('/login') do
     slim(:login)
-    db = SQLite3::Database.new('./blog.db')
-    result = db.execute("SELECT email, password FROM user")
+    db = SQLite3::Database.new("db/blog.db")
+    #db.results_as_hash = true
+
+    result = db.execute("SELECT email, password FROM user") 
+    
+
+    #db ska vara i en "post"
+end
+
+get('/login/:id') do
+    db = SQLite3::Database.new("db/blog.db")
+    db.results_as_hash = true
+
+    User_id = params["id"]
 end
 
 post('/login') do
    "You are in ^^"
+   redirect('/sign')
 end
