d19337a236c87a7c40d3e620d4e70bc3e1972677
gabriella-quintero-ntig <gabriella.quintero@elev.ga.ntig.se>
2019-05-09 10:06:44 +0200

    Dokumentering
    Har lagt "kommentarer" för att dokumentera vad funktionerna gör.

--- app.rb
+++ app.rb
@@ -2,46 +2,60 @@ require 'slim'
 require 'sinatra'
 require 'sqlite3'
 require 'bcrypt'
+require_relative 'model/model.rb'
 
 enable :session
+
+# Display landing page
 
 get('/') do 
     slim(:index)
 end
 
+# Display an about page
+
 get('/about') do
     slim(:about)
 end
 
+# Display a register form for signing in 
+
 get('/sign') do
     slim(:sign) 
+
     #db =SQLite3::Database.new("db/blog.db")
    # result = db.execute("INSERT INTO user (name, lastname, username, email, password) VALUES (' ', ' ', ' ', ' ', ' ')")
 end
+
+# Display a welcome page when register
 
 post('/sign') do
     "Welcome :)"
 end
 
+# Display a register form
+
 get('/login') do
     slim(:login)
-    db = SQLite3::Database.new("db/blog.db")
+
+    #db = SQLite3::Database.new("db/blog.db")
     #db.results_as_hash = true
 
-    result = db.execute("SELECT email, password FROM user") 
+    #result = db.execute("SELECT email, password FROM user") 
     
 
     #db ska vara i en "post"
 end
 
-get('/login/:id') do
-    db = SQLite3::Database.new("db/blog.db")
-    db.results_as_hash = true
+#get('/login/:id') do
+    #db = SQLite3::Database.new("db/blog.db")
+    #db.results_as_hash = true
 
-    User_id = params["id"]
-end
+    #User_id = params["id"]
+#end
 
-post('/login') do
-   "You are in ^^"
-   redirect('/sign') 
+# Display a welcome page when you log in
+
+post('/login') do 
+    "you are in :)"
 end
